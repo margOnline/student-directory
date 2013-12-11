0810d7b135fc4a6b4f4487e76f04080c3fe1ba77
Margo <margo@margonline.co.uk>
2013-12-11 20:23:36 +0000

    use methods

--- directory.rb
+++ directory.rb
@@ -1,5 +1,3 @@
-puts "The students in the August cohort"
-puts "--------------------"
 students = ["Dr. Hannibal Lecter",
             "Darth Vader",
             "Nurse Ratched",
@@ -9,5 +7,20 @@ students = ["Dr. Hannibal Lecter",
             "The Dark Knight",
             "Professor Green"
           ]
-students.each {|student| puts student}
-puts "Overall, we have #{students.length} great students."
+
+def print_header
+  puts "The students in the August cohort"
+  puts "--------------------"
+end
+
+def print(names)
+  names.each {|name| puts name}
+end
+
+def print_footer(students)
+  puts "Overall, we have #{students.length} great students."
+end
+
+print_header
+print(students)
+print_footer(students)
