60f3e6cbc3a4b03e934cd9f999e4a6e450e61d3e
Margo <margo@margonline.co.uk>
2013-12-11 20:18:29 +0000

    use arrays

--- directory.rb
+++ directory.rb
@@ -1,13 +1,13 @@
-student_count = 8
 puts "The students in the August cohort"
 puts "--------------------"
-puts "Dr. Hannibal Lecter"
-puts "Darth Vader"
-puts "Nurse Ratched"
-puts "Michael Corleone"
-puts "Alex De Large"
-puts "Freddy Kruger"
-puts "The Dark Knight"
-puts "Professor Green"
-#show the total
-puts "Overall, we have #{student_count} great students."
+students = ["Dr. Hannibal Lecter",
+            "Darth Vader",
+            "Nurse Ratched",
+            "Michael Corleone",
+            "Alex De Large",
+            "Freddy Kruger",
+            "The Dark Knight",
+            "Professor Green"
+          ]
+students.each {|student| puts student}
+puts "Overall, we have #{students.length} great students."
