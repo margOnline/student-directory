2b7722cb2e84dbf12dbcc552257243233a6d4252
Margo <margo@margonline.co.uk>
2013-12-11 20:46:16 +0000

    added test for no students

--- directory.rb
+++ directory.rb
@@ -1,22 +1,14 @@
-# students = [
-#             {:name => "MargOnline", :cohort => :august},
-#             {:name => "HarriSam", :cohort => :august},
-#             {:name => "LorrainePhil", :cohort => :august},
-#             {:name => "Kunks001", :cohort => :august},
-#             {:name => "MindfulCoder", :cohort => :august},
-#             {:name => "Chewymeister", :cohort => :august},
-#             {:name => "Violentr", :cohort => :august},
-#             {:name => "JamesJoshuaHill", :cohort => :august},
-#             {:name => "Ting0807", :cohort => :august}
-#           ]
-
 def print_header
-  puts "The students in the August cohort"
+  puts "Students in the August cohort"
   puts "--------------------"
 end
 
 def print(names)
-  names.each {|name| puts "#{name[:name]} in #{name[:cohort].capitalize} cohort"}
+  if names.empty?
+    puts "No students have enrolled yet."
+  else
+    names.each {|name| puts "#{name[:name]} in #{name[:cohort].capitalize} cohort"}
+  end
 end
 
 def print_footer(students)
