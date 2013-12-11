94a2e6b071d096fed494e0aad881c62d9c196070
Margo <margo@margonline.co.uk>
2013-12-11 20:11:19 +0000

    directory.rb file with list of students and count

---
+++ directory.rb
@@ -0,0 +1,16 @@
+student_count = 8
+puts "The students in the August cohort"
+puts "--------------------"
+puts "Dr. Hannibal Lecter"
+puts "Darth Vader"
+puts "Nurse Ratched"
+puts "Michael Corleone"
+puts "Alex De Large"
+puts "Freddy Kruger"
+puts "The Dark Knight"
+puts "Professor Green"
+#show the total
+print "Overall, we have "
+#print doesn't add new line characters
+print student_count
+puts " great students."
